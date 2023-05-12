29c4a9f950c34a0208c02c416d6264cbc1f11e51
Ziwei Wang <ziwei.1.wang@kcl.ac.uk>
2023-05-12 14:49:47 +0100

    trainer basic functionality

--- MLproj/revise/SimpleCNN/data.py
+++ MLproj/revise/SimpleCNN/data.py
@@ -4,7 +4,7 @@ import torchvision
 import torch
 
 
-def loadData(root="./data", batch_size=64):
+def load_data(root="../data", batch_size=64):
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
--- MLproj/revise/SimpleCNN/train.py
+++ MLproj/revise/SimpleCNN/train.py
@@ -1,9 +1,67 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from network import SimpleCNN
+from data import load_data
 
 
-def train():
-    pass
+class Trainer:
+    def __init__(self, model, parameters, train_loader):
+        self.model = model
+        self.parameters = parameters
+        self.train_loader = train_loader
+
+        self.device = self.parameters["device"]
+        self.num_epoch = self.parameters["num_epoch"]
+        self.lr = self.parameters["lr"]
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+        self.losses = []
+
+    def save_model(self, path):
+        torch.save(self.model.state_dict(), path + "/simpleCNN.pth")
+
+    def load_model(self, path):
+        self.model.load_stat_dict(torch.load(path + "/simpleCNN.pth"))
+
+    def plot(self):
+        plt.plot(self.losses)
+        plt.show()
+
+    def train(self):
+        for epoch in range(self.num_epoch):
+            train_loss = 0.0
+            for i, (images, label) in enumerate(self.train_loader):
+                images = images.to(self.device)
+                label = label.to(self.device)
+
+                self.optimizer.zero_grad()
+                predict = self.model(images)
+                loss = self.loss_function(predict, label)
+                train_loss += loss
+                if (i + 1) % 100 == 0:
+                    print(f"Epoch [{epoch + 1}/{self.num_epoch}], Step [{i + 1}], Loss: {loss.item():.4f}")
+
+                loss.backward()
+                self.optimizer.step()
+            self.losses.append(train_loss)
+
+
+def main():
+    model = SimpleCNN(num_class=10)
+    parameters = {
+        "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        "num_epoch": 10,
+        "lr": 0.001,
+    }
+
+    train_loader, val_loader, test_loader = load_data("../data")
+    trainer = Trainer(model, parameters, train_loader)
+
+    trainer.train()
 
 
 if __name__ == '__main__':
-    train()
+    main()
