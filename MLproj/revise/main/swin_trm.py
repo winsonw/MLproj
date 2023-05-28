25ca0ab30f0b82599031975b67410c8a0c90c19e
Ziwei Wang <ziwei.1.wang@kcl.ac.uk>
2023-05-28 15:24:54 +0100

    Patch merging implement

--- MLproj/revise/main/swin_trm.py
+++ MLproj/revise/main/swin_trm.py
@@ -17,3 +17,23 @@ class PatchEmbedding(nn.Module):
         return self.norm(x)
 
 
+class PatchMerging(nn.Module):
+    def __init__(self, input_resolution, in_channels, norm_layer=nn.GELU):
+        super(PatchMerging, self).__init__()
+        self.input_resolution = input_resolution
+        self.norm = norm_layer()
+        self.linear = nn.Linear(in_channels*4, in_channels*2)
+
+    def forward(self, x):
+        B, L, C = x.shape
+        x = x.view([B, self.input_resolution[0], self.input_resolution[1], C])
+        x = torch.cat([
+            x[:, ::2, ::2, :],
+            x[:, 1::2, ::2, :],
+            x[:, ::2, 1::2, :],
+            x[:, 1::2, 1::2, :],
+        ], dim=3)
+        x = x.view([B, L // 4, 4*C])
+        x = self.norm(x)
+        x = self.linear(x)
+        return x
--- MLproj/revise/main/test.py
+++ MLproj/revise/main/test.py
@@ -1,13 +1,20 @@
-from swin_trm import PatchEmbedding
+from swin_trm import PatchEmbedding, PatchMerging
 import torch
 
 
-def patch_embedding_testing():
+def patch_embedding_test():
     x = torch.rand(2, 3, 224, 224)
     model = PatchEmbedding(in_channels=3, embedding_channels=768, patch_size=16)
     output = model(x)
     assert output.shape == torch.Size([2, 196, 768])
 
+def patch_merging_test():
+    x = torch.rand(2, 196, 768)
+    model = PatchMerging(input_resolution=(14, 14), in_channels=768)
+    output = model(x)
+    assert output.shape == torch.Size([2, 49, 1536])
+
 
 if __name__ == '__main__':
-    patch_embedding_testing()
+    # patch_embedding_testing()
+    patch_merging_test()
